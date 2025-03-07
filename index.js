cf7070a37a22f9306d6e2d6bc972a773ca62fb0d
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-03-07 14:13:02 +0300

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result
+  if (distance <= 400){
+    result = "This one is on me!";
+  }
+  else if (distance > 400 && distance < 2000 ){
+    result = 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance <= 2500 ){
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500){
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let whatCity;
+  whatCity = city === 'NYC' ? 'Ok, sounds good.': 'No go.';
+  return whatCity;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result
+  switch (tip) {
+    case tip = "generous": result = 'Thank you so much.';      
+      break;
+    case tip = "not as generous": result = 'Thank you.';
+      break;
+    default: result = 'Bye.';
+      break;
+  }
+  return result;
 }
